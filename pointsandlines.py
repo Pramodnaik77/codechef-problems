dcc17205a26f789349a07b2a349eb22baf20c17e
Pramodnaik77 <100344000+Pramodnaik77@users.noreply.github.com>
2022-02-24 15:47:01 +0530

    python code

---
+++ pointsandlines.py
@@ -0,0 +1,18 @@
+t=int(input())
+while t>0:
+    l1=[]
+    l2=[]
+    n=int(input())
+    for i in range(0,n):
+        x,y=map(int,input().split())
+        l1.append(x)
+        l2.append(y)
+    s1=set(l1)
+    s2=set(l2)
+    n1=len(s1)
+    n2=len(s2)
+    ans=n1+n2
+    print(ans)
+    t-=1
+    
+    
